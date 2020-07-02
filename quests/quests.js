6078e69ec6421631613cbff20c36b3ddb9e6f9db
taplar <taplarhw@gmail.com>
2020-07-01 21:16:09 -0500

    Created the global.css file and moved the expected common settings into it.  Also added the logic for starting to display quest records

--- quests/quests.js
+++ quests/quests.js
@@ -2,15 +2,50 @@
 
 class Namespace {
     constructor () {
+        this.templates = {
+            quest: this.loadTemplate( '#questTemplate' )
+        };
+
         const quests = this.fetchJsonFile( './quests.json' );
 
         Promise.all([quests]).then(([quests]) => {
             this.quests = quests.map( it => new Quest( it ) );
+
+            this.buildQuestsDisplay();
         });
+    }
+
+    buildQuestsDisplay () {
+        const fragment = document.createDocumentFragment();
+        const maxNameLength = Math.max( ...this.quests.map( it => it.name.length ) );
+
+        this.quests.sort( ( a, b ) => a.id.localeCompare( b.id ) );
+
+        this.quests.forEach( quest => {
+            const record = document.createElement( 'span' );            
+            record.innerHTML = this.templates.quest;
+
+            const id = record.querySelector( '.id' );
+            id.innerText = quest.id;
+
+            const name = record.querySelector( '.name' );
+            name.value = quest.name;
+            name.style.width = ( maxNameLength * 8.2 ) + 'px';
+
+            record.querySelector( '.duration' ).value = quest.duration;
+
+            fragment.appendChild( record.firstChild );
+        } );
+
+        document.querySelector( '.app .content .quests .tbody' ).appendChild( fragment );
     }
 
     fetchJsonFile ( path ) {
         return fetch( new Request( path ) ).then( response => response.json() );
+    }
+
+    loadTemplate ( selector ) {
+        return document.querySelector( selector ).innerHTML.split( '\n' ).map( it => it.trim() ).join( '' );
     }
 }
 
