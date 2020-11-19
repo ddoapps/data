58fb3aad77b25c8887dc5f4a75afbdbc03d8541c
taplar <taplarhw@gmail.com>
2020-11-18 20:32:36 -0600

    Added logic to create new quests and to export the quests to a file

--- quests/quests.js
+++ quests/quests.js
@@ -6,7 +6,7 @@
                 this.fetchJson( '../packs/packs.json' ),
                 this.fetchJson( '../sagas/sagas.json' )
             ] ).then( ( [ quests, packs, sagas ] ) => {
-                this.quests = quests.map( Quest.builder );
+                this.quests = quests.sort( ( a, b ) => a.name.localeCompare( b.name ) ).map( Quest.builder );
                 this.packs = packs;
                 this.sagas = sagas;
 
@@ -20,6 +20,10 @@
             }, {} );
         }
 
+        bindOnExportQuests () {
+            document.querySelector( '#export' ).addEventListener( 'click', _ => this.exportQuests() );
+        }
+
         bindOnFilterInput () {
             let throttle = null;
 
@@ -31,6 +35,58 @@
 
                     this.rebuildQuestSelect();
                 }, 300 );
+            } );
+        }
+
+        bindOnQuestAdd () {
+            document.querySelector( '.add-quest' ).addEventListener( 'click', _ => {
+                const questElement = document.querySelector( '.new-quest .quest' );
+                const nameElement = questElement.querySelector( 'input.name' );
+                const id = nameElement.value.toLowerCase().replace( /[^a-z]/g, '' ).trim();
+
+                if ( id && !this.quests.find( quest => quest.id === id ) ) {
+                    const newQuestData = { id };
+
+                    newQuestData.name = nameElement.value.trim();
+                    newQuestData.duration = questElement.querySelector( '.duration' ).value;
+
+                    [ 'heroic', 'epic' ].forEach( type => {
+                        const questTypeElement = questElement.querySelector( `.${type}` );
+
+                        [ 'casual', 'normal', 'hard', 'elite' ].forEach( difficulty => {
+                            const level = parseInt( questTypeElement.querySelector( `.${difficulty} .level` ).value, 10 );
+                            const xp = parseInt( questTypeElement.querySelector( `.${difficulty} .xp` ).value, 10 );
+
+                            if ( level > 0 && xp > -1 ) {
+                                newQuestData[ type ] = newQuestData[ type ] || {};
+                                newQuestData[ type ][ difficulty ] = { level, xp };
+                            }
+                        } );
+                    } );
+
+                    this.quests.push( new Quest( newQuestData ) );
+                    this.quests.sort( ( a, b ) => a.name.localeCompare( b.name ) );
+                    this.rebuildQuestSelect();
+
+                    document.querySelector( '.clear-quest' ).click();
+                }
+            } );
+        }
+
+        bindOnQuestClear () {
+            document.querySelector( '.clear-quest' ).addEventListener( 'click', _ => {
+                const questElement = document.querySelector( '.new-quest .quest' );
+
+                questElement.querySelector( '.name' ).innerText = '';
+                questElement.querySelector( 'input.name' ).value = '';
+                questElement.querySelector( '.duration' ).value = '';
+
+                [ 'heroic', 'epic' ].forEach( type => {
+                    [ 'casual', 'normal', 'hard', 'elite' ].forEach( difficulty => {
+                        questElement.querySelector( `.${type} .${difficulty} .level` ).value = -1;
+                        questElement.querySelector( `.${type} .${difficulty} .xp` ).value = -1;
+                    } );
+                } );
             } );
         }
 
@@ -48,6 +104,7 @@
             const questElement = document.querySelector( '.display .quest' );
             const quest = this.quests.find( it => it.id === id ) || new Quest( {} );
 
+            questElement.dataset.id = quest.id || '';
             questElement.querySelector( '.name' ).innerText = quest.name || '';
             questElement.querySelector( 'input.name' ).value = quest.name || '';
             questElement.querySelector( '.duration' ).value = quest.duration || '';
@@ -58,10 +115,23 @@
                 [ 'casual', 'normal', 'hard', 'elite' ].forEach( difficulty => {
                     const questDifficulty = questType[ difficulty ] || new QuestDifficulty( {} );
 
-                    questElement.querySelector( `.${type} .${difficulty} .level` ).value = questDifficulty.level || 0;
-                    questElement.querySelector( `.${type} .${difficulty} .xp` ).value = questDifficulty.xp || 0;
+                    questElement.querySelector( `.${type} .${difficulty} .level` ).value = questDifficulty.level || -1;
+                    questElement.querySelector( `.${type} .${difficulty} .xp` ).value = questDifficulty.xp || -1;
                 } );
             } );
+        }
+
+        exportQuests () {
+            const downloadLink = document.createElement( 'a' );
+            const data = JSON.stringify( this.quests.sort( ( a, b ) => a.id.localeCompare( b.id ) ).map( it => it.toMap() ) );
+
+            downloadLink.download = `quests.json`;
+            downloadLink.href = 'data:application/json;charset=utf-8,' + encodeURIComponent( data );
+            downloadLink.style.display = 'none';
+
+            document.body.appendChild( downloadLink );
+            downloadLink.click();
+            document.body.removeChild( downloadLink );
         }
 
         fetchJson ( url ) {
@@ -71,9 +141,15 @@
         initialize () {
             this.filteredQuests = this.quests;
 
+            document.querySelector( '.existing-quest' ).innerHTML = this.templates.quest.innerHTML;
+            document.querySelector( '.new-quest' ).innerHTML = this.templates.quest.innerHTML;
+
             this.rebuildQuestSelect();
             this.bindOnFilterInput();
             this.bindOnQuestSelection();
+            this.bindOnQuestClear();
+            this.bindOnQuestAdd();
+            this.bindOnExportQuests();
         }
 
         rebuildQuestSelect () {
@@ -107,6 +183,16 @@
                 data[ type ] ? this[ type ] = QuestType.builder( data[ type ] ) : null
             );
         }
+
+        toMap () {
+            const map = { id: this.id, name: this.name, duration: this.duration };
+
+            [ 'heroic', 'epic' ].forEach( type => {
+                if ( this[ type ] ) map[ type ] = this[ type ].toMap();
+            } );
+
+            return map;
+        }
     }
 
     class QuestType {
@@ -117,6 +203,16 @@
                 data[ difficulty ] ? this[ difficulty ] = QuestDifficulty.builder( data[ difficulty ] ) : null
             );
         }
+
+        toMap () {
+            const map = {};
+
+            [ 'casual', 'normal', 'hard', 'elite' ].forEach( difficulty => {
+                if ( this[ difficulty ] ) map[ difficulty ] = this[ difficulty ].toMap();
+            } );
+
+            return map;
+        }
     }
 
     class QuestDifficulty {
@@ -124,6 +220,10 @@
 
         constructor ( data ) {
             [ 'level', 'xp' ].forEach( key => this[ key ] = data[ key ] );
+        }
+
+        toMap () {
+            return { level: this.level, xp: this.xp };
         }
     }
 }() );
